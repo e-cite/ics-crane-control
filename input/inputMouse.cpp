b67a991d2dc65fbc324ab28be8ef3776ffde8519
Andreas Dolp <andreasdolp1991@gmail.com>
2014-04-17 00:08:14 +0200

    Fertigstellung des Konstruktors, erste komplette Programmierung der inputMouse::read() Noch testen und kommentieren!

--- input/inputMouse.cpp
+++ input/inputMouse.cpp
@@ -2,9 +2,7 @@
  * Projekt: ICS - Kran Neubau
  * Dateiname: inputMouse.cpp
  * Funktion: Implementierung der Klasse inputMouse, Programmierung der Funktionen
- * Kommentar: Erste Version
- * Parameter:
- * Rueckgabe:
+ * Kommentar: Fertigstellung des Konstruktors, erste komplette Programmierung der inputMouse::read()
  * Name: Andreas Dolp
  * Datum: 09.04.2014
  * Version: 0.1
@@ -16,11 +14,34 @@
 #include <iostream>			/* cout */
 using namespace std;
 
-inputMouse::inputMouse(const char* cpMousePathToSet) {
+inputMouse::inputMouse(const char* cpMousePathToSet)
+	: cpMousePath{*cpMousePathToSet} {
 }
 
 
 bool inputMouse::read() {
+	int fd;
+	char buffer[SIZE_OF_MOUSE_DATA];
+	if (( fd = open(this->cpMousePath, O_RDONLY)) == -1) {
+		throw EXCEPTION_UNABLE_READ_MOUSE;
+		return false;
+	} else {
+		::read(fd, &buffer, SIZE_OF_MOUSE_DATA);
+		this->bClickLeft = (buffer[0] & 0b00000001) >> 0;
+		if ((buffer[0] & 0b00000001) >> 0)
+			cout << "Maus links \n";
+
+		this->bClickRight = (buffer[0] & 0b00000010) >> 1;
+		if ((buffer[0] & 0b00000010) >> 1)
+			cout << "Maus rechts \n";
+
+		this->bClickMiddle = (buffer[0] & 0b00000100) >> 2;
+		if ((buffer[0] & 0b00000100) >> 2)
+			cout << "Maus mitte \n";
+			cout << "X: " << buffer[1] << "\n";
+			cout << "Y: " << buffer[2] << "\n";
+		}
+		close(fd);
 	return false;
 }
 
--- input/inputMouse.h
+++ input/inputMouse.h
@@ -2,9 +2,7 @@
  * Projekt: ICS - Kran Neubau
  * Dateiname: inputMouse.h
  * Funktion: Header zu inputMouse.cpp, public-Ableitung der Klasse inputMouse von inputMovement. Definiert die noetigen Funktionen zum Einlesen einer USB-Maus
- * Kommentar: Erste Version
- * Parameter:
- * Rueckgabe:
+ * Kommentar: Fertigstellung des Konstruktors, erste komplette Programmierung der inputMouse::read()
  * Name: Andreas Dolp
  * Datum: 09.04.2014
  * Version: 0.1
@@ -13,6 +11,12 @@
 #ifndef INPUTMOUSE_H_
 #define INPUTMOUSE_H_
 
+#define SIZE_OF_STRING_LENGTH 100
+#define SIZE_OF_MOUSE_DATA 3
+
+#define EXCEPTION_UNABLE_READ_MOUSE 1
+
+
 #include "inputMovement.h"
 
 
@@ -20,6 +24,9 @@ class inputMouse: public inputMovement {
 public:
 	inputMouse(const char* cpMousePathToSet);
 	bool read();
+
+private:
+	const char cpMousePath[SIZE_OF_STRING_LENGTH];
 };
 
 #endif /* INPUTMOUSE_H_ */
