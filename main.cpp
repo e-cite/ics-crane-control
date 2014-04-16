d8a190f41156bd607e23335f0b4a23e2e8a3aac9
Andreas Dolp <andreasdolp1991@gmail.com>
2014-04-16 23:27:09 +0200

    Erstellung des Projekts, Erste Version der main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*---------------------------
+ * Projekt: ICS - Kran Neubau
+ * Dateiname: main.cpp
+ * Funktion: Hauptprojekt
+ * Kommentar: Erste Version
+ * Name: Andreas Dolp
+ * Datum: 16.04.2014
+ * Version: 0.1
+ ---------------------------*/
+
+#include "main.h"
+#include <cstdio>	/* printf */
+
+int main ( int argc, char* argv[] ) {
+	printf("ICS - Crane Control\n");
+	printf("============================================================\n");
+	printf("Version %G\n", VERSION);
+	printf("============================================================\n");
+	printf("Written by Andreas Dolp for ICS - Innovative Crane Solutions\n");
+	printf("This software is licensed under GNU GPLv3\n");
+	printf("============================================================\n");
+
+	return 0;
+}
+
+
---
+++ main.h
@@ -0,0 +1,18 @@
+/*---------------------------
+ * Projekt: ICS - Kran Neubau
+ * Dateiname: main.h
+ * Funktion: Header zu main.cpp
+ * Kommentar: Erste Version, Angabe der Versionsnummer
+ * Name: Andreas Dolp
+ * Datum: 16.04.2014
+ * Version: 0.1
+ ---------------------------*/
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define VERSION 0.1
+
+int main ( int argc, char* argv[] );
+
+#endif /* MAIN_H_ */
