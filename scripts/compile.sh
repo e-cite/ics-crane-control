f2d754d2b04a795a1458e9012f514b42da7bd117
Andreas Dolp <andreasdolp1991@gmail.com>
2014-06-10 12:57:11 +0200

    mkdir-Anweisung in compile.sh eingefügt

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,5 @@
+mkdir ../bin
+
 g++ ../src/*.cpp ../src/*/*.cpp -o ../bin/ics-crane-control_release
 g++ ../src/*.cpp ../src/*/*.cpp -pthread -std=c++0x -D DEBUG -o ../bin/ics-crane-control_printf
 g++ ../src/*.cpp ../src/*/*.cpp -pthread -std=c++0x -lncurses -D NCURSES -o ../bin/ics-crane-control_ncurses
